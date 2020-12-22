fe40f52291e97455553dcd541d0bcfbe380be102
Jesse Bounds <jesse@rebounds.net>
2020-12-21 20:32:44 -0800

    Start setting up index page

---
+++ components/layout.js
@@ -0,0 +1,38 @@
+import Head from 'next/head'
+
+export const siteTitle = "bounds.dev"
+
+export default function Layout({ children, home }) {
+  return (
+    <div>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>
+          {siteTitle}
+        </title>
+        <meta
+          name="description"
+          content="Personal website of Jesse Bounds"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.now.sh/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header>
+        {home ? (
+          <>
+          </>
+        ) : (
+          <>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+    </div>
+  )
+}
